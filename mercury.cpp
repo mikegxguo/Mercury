78a1d8e7548aeb2a724b3681a7bd58749cd65957
mike.gou <gxgou.gou@mic.com.tw>
2016-09-09 13:24:53 +0800

    [Font] Add the charactor;

--- mercury.cpp
+++ mercury.cpp
@@ -260,10 +260,10 @@ const DataDef FontDef[FontNum]={
 		{424699, "xbf/Verdanaedit22_19.xbf"}, //
 		{433695, "xbf/Verdanaedit22_23.xbf"}, //
 		{458940, "xbf/Verdanaedit22_29.xbf"}, //
-		{362655, "xbf/MSPGothic_15.xbf"}, //
-		{455724, "xbf/MSPGothic_19.xbf"}, //
-		{497286, "xbf/MSPGothic_23.xbf"}, //
-		{658841, "xbf/MSPGothic_29.xbf"}, //
+		{362689, "xbf/MSPGothic_15.xbf"}, //
+		{455785, "xbf/MSPGothic_19.xbf"}, //
+		{497359, "xbf/MSPGothic_23.xbf"}, //
+		{658961, "xbf/MSPGothic_29.xbf"}, //
 		{3848, "xbf/Verdanaedit22_40.xbf"}, //1
 		{7427, "xbf/Verdanaedit22_59.xbf"}, //2
 };
